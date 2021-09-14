a6258b1e2e6c2a057680deab56b18a46cf4f05bb
Соколовский Роман Иванович <vc0042@mccd.mcc.krasnoyarsk.su>
2021-09-14 10:49:42 +0700

    Initial commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div className="app-content">
+            <div>
+                <img src="https://im0-tub-ru.yandex.net/i?id=0e1b789955094b4a2b3508762bd2e20c&ref=rim&n=33&w=291&h=164" alt="banner" />
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                my post
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
