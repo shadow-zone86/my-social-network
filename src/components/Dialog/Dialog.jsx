ffbfb9fdeebc3b4acaa5219866f776280897f8e6
Соколовский Роман Иванович <vc0042@mccd.mcc.krasnoyarsk.su>
2021-09-15 17:05:18 +0700

    UI - BLL

--- src/components/Dialog/Dialog.jsx
+++ src/components/Dialog/Dialog.jsx
@@ -3,20 +3,34 @@ import Message from './Message/Message'
 import Users from './Users/Users'
 import classes from './Dialog.module.css'
 
+let dataUser = [
+    { id: 1, name: 'Shadow zone' },
+    { id: 2, name: 'The doctor' },
+    { id: 3, name: 'Wolf' },
+    { id: 4, name: 'Thunder' },
+    { id: 5, name: 'Porsche' }
+]
+
+let dataMessage = [
+    { id: 1, message: 'Angular.js' },
+    { id: 2, message: 'React.js' },
+    { id: 3, message: 'Vue.js' }
+]
+
 const Dialog = () => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.users}>
-                <Users name='Shadow zone' id='1' />
-                <Users name='The doctor' id='2' />
-                <Users name='Wolf' id='3' />
-                <Users name='Thunder' id='4' />
-                <Users name='Porsche' id='5' />
+                <Users name={dataUser[0].name} id={dataUser[0].id} />
+                <Users name={dataUser[1].name} id={dataUser[1].id} />
+                <Users name={dataUser[2].name} id={dataUser[2].id} />
+                <Users name={dataUser[3].name} id={dataUser[3].id} />
+                <Users name={dataUser[4].name} id={dataUser[4].id} />
             </div>
             <div className={classes.messages}>
-                <Message message='Angular.JS' />
-                <Message message='React.JS' />
-                <Message message='Vue.JS' />
+                <Message message={dataMessage[0].message} />
+                <Message message={dataMessage[1].message} />
+                <Message message={dataMessage[2].message} />
             </div>
         </div>
     )
--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Post from './Post/Post'
 import classes from './Posts.module.css'
 
+let dataPost = [
+    { id: 1, message: "Hello world", image: "https://im0-tub-ru.yandex.net/i?id=7abd6330d0e762b7004822896c8ab501&n=13&exp=1", like: 15 },
+    { id: 2, message: "It's my life", image: "https://i.pinimg.com/originals/ae/1e/e3/ae1ee36bc38cebcc8d3d9f1691b715dd.jpg", like: 20 }
+]
+
 const Posts = () => {
     return (
         <div className={classes.description}>
@@ -15,8 +20,8 @@ const Posts = () => {
                 </div>
             </div>
             <div className={classes.posts}>
-                <Post message="Hello world!" image="https://im0-tub-ru.yandex.net/i?id=7abd6330d0e762b7004822896c8ab501&n=13&exp=1" like="15" />
-                <Post message="It's my life" image="https://i.pinimg.com/originals/ae/1e/e3/ae1ee36bc38cebcc8d3d9f1691b715dd.jpg" like="20" />
+                <Post message={dataPost[0].message} image={dataPost[0].image} like={dataPost[0].like} />
+                <Post message={dataPost[1].message} image={dataPost[1].image} like={dataPost[1].like} />
             </div>
         </div>
     )
