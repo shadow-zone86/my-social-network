10d6dd8fb5f8fd235f5f048b5bad4320b05cec57
Соколовский Роман Иванович <vc0042@mccd.mcc.krasnoyarsk.su>
2021-09-15 11:47:31 +0700

    Making up the dialogs page

--- src/components/Dialog/Dialog.jsx
+++ src/components/Dialog/Dialog.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
-//import classes from './Dialog.module.css'
+import classes from './Dialog.module.css'
 
 const Dialog = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.users}>
+                <div className={classes.item + ' ' + classes.active}>
+                    User 1
+                </div>
+                <div className={classes.item}>
+                    User 2
+                </div>
+                <div className={classes.item}>
+                    User 3
+                </div>
+                <div className={classes.item}>
+                    User 4
+                </div>
+                <div className={classes.item}>
+                    User 5
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>
+                    Comment 1
+                </div>
+                <div className={classes.message}>
+                    Comment 2
+                </div>
+                <div className={classes.message}>
+                    Comment 3
+                </div>
+            </div>
         </div>
     )
 }
