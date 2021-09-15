d472ba7ff632905b97dd2e0339fd417208ba6ac8
Соколовский Роман Иванович <vc0042@mccd.mcc.krasnoyarsk.su>
2021-09-15 08:53:52 +0700

    Props

--- src/components/Profile/Posts/Post/Post.jsx
+++ src/components/Profile/Posts/Post/Post.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import classes from './Post.module.css'
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={classes.item}>
-            <img src='https://im0-tub-ru.yandex.net/i?id=7abd6330d0e762b7004822896c8ab501&n=13&exp=1' />
-            Hello World
+            <img src={props.image} />
+            {props.message}
             <div>
-                <span>like</span>
+                <span className={`${classes.item} ${classes.like}`}>like - {props.like}</span>
             </div>
         </div>
     )
--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -10,8 +10,9 @@ const Posts = () => {
                 <textarea></textarea>
                 <button>Add post</button>
             </div>
-            <div>
-                <Post />
+            <div className={classes.posts}>
+                <Post message="Hello world!" image="https://im0-tub-ru.yandex.net/i?id=7abd6330d0e762b7004822896c8ab501&n=13&exp=1" like="15" />
+                <Post message="It's my life" image="https://i.pinimg.com/originals/ae/1e/e3/ae1ee36bc38cebcc8d3d9f1691b715dd.jpg" like="20" />
             </div>
         </div>
     )
