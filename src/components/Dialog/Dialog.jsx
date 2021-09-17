ebfba232c8efdb98e263a0ba806bec473121bb1a
Соколовский Роман Иванович <vc0042@mccd.mcc.krasnoyarsk.su>
2021-09-17 09:54:22 +0700

    onClick, ref, VirtualDOM for page messages

--- src/components/Dialog/Dialog.jsx
+++ src/components/Dialog/Dialog.jsx
@@ -6,22 +6,31 @@ import classes from './Dialog.module.css'
 const Dialog = (props) => {
     let users = props.dataUser.map((user, index) => {
         return (
-            <Users name={user.name} id={user.id} key={index} />
+            <Users name={ user.name } id={ user.id } key={ index } />
         )
     })
-
     let messages = props.dataMessage.map((message, index) => {
         return (
-            <Message message={message.message} key={index} />
+            <Message message={ message.message } key={ index } />
         )
     })
-
+    let newMessageElement = React.createRef();
+    let addMessage = () => {
+        let text = newMessageElement.current.value
+        window.alert(text)
+    }
     return (
-        <div className={classes.dialogs}>
-            <div className={classes.users}>
+        <div className={ classes.dialogs }>
+            <div className={ classes.users }>
                 { users }
             </div>
-            <div className={classes.messages}>
+            <div className={ classes.messages }>
+                <div>
+                    <textarea ref={ newMessageElement }></textarea>
+                </div>
+                <div>
+                    <button onClick={ addMessage }>Add message</button>
+                </div>
                 { messages }
             </div>
         </div>
--- src/components/Dialog/Message/Message.jsx
+++ src/components/Dialog/Message/Message.jsx
@@ -3,8 +3,10 @@ import classes from './Message.module.css'
 
 const Message = (props) => {
     return (
-        <div className={classes.message}>
-            {props.message}
+        <div>
+            <div className={ classes.message }>
+                { props.message }
+            </div>
         </div>
     )
 }
